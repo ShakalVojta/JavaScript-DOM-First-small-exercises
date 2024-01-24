09a5d2ec6ff4bae6f796279edd9f92aecd880c0e
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-01-24 20:24:42 +0100

    Complete second challenge

--- proper-login/script.js
+++ proper-login/script.js
@@ -1,10 +1,29 @@
-const btn = document.querySelector('.btn')
-const password = document.querySelector('.password')
+const tryLogin = () => {
 
-btn.addEventListener('click', () => {
-    if (password === 'bootcamp') {
-        alert('Welcome')
+    const email = document.querySelector('input[name="email"]').value;
+
+    const password = document.querySelector('input[name="password"]').value;
+
+    if (email === 'bruce@wayne.com' && password === 'ILoveCats') {
+        displaySuccess('Login successful');
     } else {
-        alert('You are loooser!!!!')
+        displayError('Unknown user or incorrect password');
     }
-})
+
+}
+
+const displayError = (message) => {
+
+    const messagesContainer = document.querySelector('.login-form__messages');
+
+    messagesContainer.innerHTML = '<div class="login-form__message login-form__message_error">'+message+'</div>';
+
+}
+
+const displaySuccess = (message) => {
+
+    const messagesContainer = document.querySelector('.login-form__messages');
+
+    messagesContainer.innerHTML = '<div class="login-form__message login-form__message_success">'+message+'</div>';
+
+}
